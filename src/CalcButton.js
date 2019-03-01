e88c26ac7dcd4284ad2fc7234456b70560598a2b
verzion2k <kovacs.zsoltzx@gmail.com>
2019-03-01 13:50:59 +0100

    finished the layout borders are left

--- src/CalcButton.js
+++ src/CalcButton.js
@@ -23,9 +23,12 @@ class CalcButton extends Component {
         <button className="calc__button">1</button>
         <button className="calc__button">2</button>
         <button className="calc__button">3</button>
-        <button className="calc__button calc__button--equal calc__button--orange calc__button--higher">
-          =
-        </button>
+
+        <div className="column">
+          <button className="calc__button calc__button--equal calc__button--orange calc__button--higher">
+            =
+          </button>
+        </div>
 
         <button className="calc__button calc__button--wider">0</button>
         <button className="calc__button">,</button>
