4d7483e7d848bb53ad4720e82a49887e9ab355e0
verzion2k <kovacs.zsoltzx@gmail.com>
2019-03-01 14:10:14 +0100

    layout finished

--- src/Calc.js
+++ src/Calc.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import CalcSum from "./CalcSum";
-import CalcButtonList from "./CalcButtonList"
-import './Calc.scss';
+import CalcButtonList from "./CalcButtonList";
+import "./Calc.scss";
 
 class Calc extends Component {
   render() {
     return (
-      <>
-      <CalcSum />
-      <CalcButtonList />
-      </>
-    )
+      <div className="calc__wrapper">
+        <CalcSum />
+        <CalcButtonList />
+      </div>
+    );
   }
 }
 
